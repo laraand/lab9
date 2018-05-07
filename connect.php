9b469230f43e1809dd1fd9e9a03575496a6f01be
Andrea Lara <anlara@csumb.edu>
2018-05-07 17:37:59 +0000

    updating data

--- connect.php
+++ connect.php
@@ -1,15 +1,16 @@
 <?php
 
-function getDBConnection() {
+function getDBConnection($dbName) {
     
-    //C9 db info
+    //C9 db info 
+    //mysql://bdc992bb9af150:29b5a64a@us-cdbr-iron-east-04.cleardb.net/heroku_981fc0aa0d1b350?reconnect=true
     $host = "us-cdbr-iron-east-04.cleardb.net";
-    $dbName = "heroku_981fc0aa0d1b350";
+    $db = $dbName;
     $username = "bdc992bb9af150";
     $password = "29b5a64a";
     
     //when connecting from Heroku
-    //mysql://bdc992bb9af150:29b5a64a@us-cdbr-iron-east-04.cleardb.net/heroku_981fc0aa0d1b350?reconnect=true
+   
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         $host = $url["host"];
--- verifyUser.php
+++ verifyUser.php
@@ -2,7 +2,7 @@
 session_start();
 
 include 'connect.php';
-$connect = getDBConnection();
+$connect = getDBConnection("csumb_quiz");
 
 //Checking credentials in database
 
